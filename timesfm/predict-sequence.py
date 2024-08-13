b6be754821769a6bf8f2f60fb3955a26eae82fd6
Bryce Allen <bdallen@uchicago.edu>
2024-08-13 13:10:42 -0500

    WIP timesfm

---
+++ timesfm/predict-sequence.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+from itertools import islice
+import os.path
+
+import numpy as np
+import pandas as pd
+
+from matplotlib import pyplot as plt
+import matplotlib.dates as mdates
+
+import timesfm
+
+
+def main(inpath, outpath, prediction_length = 24, backend="gpu"):
+    values = np.loadtxt(inpath, dtype=np.float32)
+    train = values[:-prediction_length]
+    dates = pd.date_range("2024-01-01", periods=len(values), freq="1H")
+    df = pd.DataFrame({"y":values, "ds": dates})
+    df["unique_id"] = 0
+    print("cols", df.columns)
+    df_train = df[:-prediction_length]
+    n_cycles = len(df) / prediction_length
+
+    tfm = timesfm.TimesFm(
+        context_len=512,
+        horizon_len=prediction_length * 4,
+        input_patch_len=32,
+        output_patch_len=128,
+        num_layers=20,
+        model_dims=1280,
+        backend=backend,
+    )
+        #train_state_unpadded_shape_dtype_struct
+    tfm.load_from_checkpoint(repo_id="google/timesfm-1.0-200m")
+
+    df_future = tfm.forecast_on_df(inputs=df_train, freq="H", value_name="y",
+                                   num_jobs=-1)
+
+    history_tail = df[-min(4*prediction_length, len(values)):]
+    forecast_index = range(len(history_tail) - prediction_length, len(history_tail))
+
+    plt.figure(figsize=(8, 4))
+    plt.plot(history_tail, color="royalblue", label="historical data")
+    plt.plot(df_future, color="tomato", label="forecast")
+    plt.legend()
+    plt.grid()
+    #plt.show()
+    plt.savefig(outpath)
+
+
+if __name__ == "__main__":
+    import sys
+    inpath = sys.argv[1]
+    outpath, _ = os.path.splitext(inpath)
+    outpath += "-timesfm.pdf"
+    main(inpath, outpath)
+    print(outpath)
