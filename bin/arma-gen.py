62ef8276235bc75b513c5e5f5f278bd0b47c6c8b
Bryce Allen <bdallen@uchicago.edu>
2024-08-14 10:29:12 -0400

    add pandas output option for arma gen

--- bin/arma-gen.py
+++ bin/arma-gen.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
+import os.path
+
 import numpy as np
 import pandas as pd
-import statsmodels.tsa.api as tsa
+import statsmodels.api as sm
 
 
 SEED = 42
@@ -10,18 +12,50 @@ SEED = 42
 def main():
     np.random.seed(SEED)
 
-    args = get_args()
+    parser = get_arg_parser()
+    args = parser.parse_args()
 
     y = arma_generate(args.count, args.phi, args.theta,
                       scale=args.scale_deviation, mean=args.mean)
 
+    df = None
+    if args.frequency:
+        start_date = "2024-01-01"
+        dates = pd.date_range(start_date, freq=args.frequency,
+                              periods=args.count)
+        # Note: use column names that model libraries expect. Some libraries
+        # may still require renaming to work.
+        df = pd.DataFrame({ "target": y, "ds": dates })
+        df["frequency"] = args.frequency
+
     if args.output_file:
-        np.savetxt(args.output_file, y)
+        if args.frequency:
+            _, ext = os.path.splitext(args.output_file)
+            if ext == ".csv":
+                df.to_csv(args.output_file)
+            elif ext == ".parquet":
+                df.to_parquet(args.output_file)
+            elif ext == ".hdf":
+                df.to_hdf(args.output_file)
+            elif ext == ".pickle":
+                df.to_pickle(args.output_file)
+            else:
+                parser.error("Unknown output format for pandas: '%s'" % ext)
+        else:
+            if ext == ".txt" or ext == "":
+                np.savetxt(args.output_file, y)
+            elif ext == ".npy":
+                np.save(args.output_file, y)
+            else:
+                parser.error("Unknown output format for numpy: '%s'" % ext)
     else:
-        print(y[:10])
-        if len(y) > 20:
-            print("...")
-            print(y[-10:])
+        if args.frequency:
+            print(df)
+        else:
+            print(y[:10])
+            if len(y) > 20:
+                print("...")
+                print(y[-10:])
 
 
 def arma_generate(n, phi, theta, scale=1.0, mean=0):
@@ -29,16 +63,13 @@ def arma_generate(n, phi, theta, scale=1.0, mean=0):
     # practice this means we need to pass 1 and negate the ar params.
     arparams = np.r_[1, -np.array(phi)]
     maparams = np.r_[1, np.array(theta)]
-    y = tsa.arma_generate_sample(arparams, maparams,
-                                 nsample=n, scale=scale)
+    y = sm.tsa.arma_generate_sample(arparams, maparams,
+                                    nsample=n, scale=scale)
+    y += mean
     return y
-    #y += mean * (1 - np.sum(phi) - np.sum(theta))
-
-    dates = pd.date_range(start_date, freq=frequency, periods=n)
-    return pd.DataFrame(index=dates, data=y, columns=["value"])
 
 
-def get_args():
+def get_arg_parser():
     import argparse
     parser = argparse.ArgumentParser(
                 description="generate ARMA dataframe")
@@ -53,7 +84,9 @@ def get_args():
     parser.add_argument("-s", "--scale-deviation", type=float, default=1,
                         help="white noise standard deviation (default 1)")
     parser.add_argument("-o", "--output-file", required=False)
-    return parser.parse_args()
+    parser.add_argument("-f", "--frequency",
+        help="output a dataframe with time column of specified frequency (H, D, M, Y)")
+    return parser
 
 
 if __name__ == "__main__":
